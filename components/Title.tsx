617f68cc3f7b7992249229c67ec355e6cc4bc26f
batorgilm <batorgil.m@nest.edu.mn>
2022-11-22 14:42:57 +0800

    add Title interface

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Title = ({ children }) => {
-  return <h1 className="text-3xl font-bold underline">{children}</h1>;
+export interface Title {
+  text: string;
+}
+
+const Title = ({ text }: Title) => {
+  return <h1 className="text-3xl font-bold underline">{text}</h1>;
 };
 
 export default Title;
